325aaea078730c46030f917c060c17785be14cf9
Kissor Jeyabalan <kij@kij.no>
2022-12-19 15:55:11 +0100

    feat: add media models

---
+++ domain/base/src/main/kotlin/no/kij/tsuki/domain/base/model/entry/MediaEntry.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2022 Kissor Jeyabalan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package no.kij.tsuki.domain.base.model.entry
+
+import java.time.LocalDateTime
+
+sealed class MediaEntry(
+    val id: Int,
+    val title: String,
+    val coverImage: String,
+    val format: CommonMediaEntry.Format
+) {
+    constructor(entry: CommonMediaEntry) : this(
+        id = entry.id,
+        title = entry.title,
+        coverImage = entry.coverImage,
+        format = entry.format
+    )
+
+    data class Anime(
+        val entry: CommonMediaEntry,
+        val episodes: Int?,
+        val nextEpisode: NextEpisode?
+    ) : MediaEntry(entry) {
+        data class NextEpisode(
+            val number: Int,
+            val at: LocalDateTime
+        )
+    }
+
+    data class Manga(
+        val entry: CommonMediaEntry,
+        val chapters: Int?,
+        val volume: Int?
+    ) : MediaEntry(entry)
+}
